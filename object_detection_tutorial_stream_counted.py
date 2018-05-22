895a613d68fe0aacf569bf3218cd3d6d0bacbd44
Keenan Gebze <keenan.gebze@gmail.com>
2018-05-22 15:26:14 +0700

    Using 4 space indentation

--- object_detection_tutorial_stream_counted.py
+++ object_detection_tutorial_stream_counted.py
@@ -8,83 +8,86 @@ import cv2
 
 from services import CCTVService
 
-# Start Web Service
-cond = threading.Condition()
-service = CCTVService(cond, b'', {})
-server = threading.Thread(target=cherrypy.quickstart, args=[service])
-server.start()
+def main():
+    # Start Web Service
+    cond = threading.Condition()
+    service = CCTVService(cond, b'', {})
+    server = threading.Thread(target=cherrypy.quickstart, args=[service])
+    server.start()
 
-sys.path.append("..")
-from utils import label_map_util
-from utils import visualization_utils as vis_util
+    sys.path.append("..")
+    from utils import label_map_util
+    from utils import visualization_utils as vis_util
 
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-NUM_CLASSES = 90
+    MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+    PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+    PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+    NUM_CLASSES = 90
 
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
 
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
 
-cap = cv2.VideoCapture('http://114.110.17.6:8896/image.jpg?type=motion&camera=1')
+    cap = cv2.VideoCapture('http://114.110.17.6:8896/image.jpg?type=motion&camera=1')
 
-with detection_graph.as_default():
-  with tf.Session(graph=detection_graph) as sess:
-    while True:
-      ret, image_np = cap.read()
-      image_np_expanded = np.expand_dims(image_np, axis=0)
-      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-      scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
-      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-      # Actual detection.
-      (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
-      # Visualization of the results of a detection.
-      vis_util.visualize_boxes_and_labels_on_image_array(
-          image_np,
-          np.squeeze(boxes),
-          np.squeeze(classes).astype(np.int32),
-          np.squeeze(scores),
-          category_index,
-          use_normalized_coordinates=True,
-          line_thickness=2)
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            while True:
+            ret, image_np = cap.read()
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            # Actual detection.
+            (boxes, scores, classes, num_detections) = sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=2)
+            # yang ini buat count objectnya
+            # 1: person, 2: bicycle, 3: car, 4: motorcycle, 6: bus, 8: truck
+            data = {}  
+            data['vessel'] = []
+            s_class = classes[scores > 0.5]
+            # print(str(len(s_class))+ ' object')
+            # for i in range(len(s_class)): print(category_index.get(s_class[i])['name'])
+            if(len(s_class)!=0 and s_class[0] == 3):
+                cv2.putText(image_np,"Detected Car: " + str(len(s_class)), (10, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255),2,cv2.FONT_HERSHEY_SIMPLEX)
+                # save output to json
+                data['vessel'].append({  
+                    'id': 3,
+                    'object': 'car',
+                    'sum': str(len(s_class))
+                })
+            elif(len(s_class)!=0 and s_class[0] == 1):
+                cv2.putText(image_np,"Detected Person: " + str(len(s_class)), (10, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255),2,cv2.FONT_HERSHEY_SIMPLEX)
+            else:
+                cv2.putText(image_np,"Detected Object: " + str(len(s_class)), (10, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255),2,cv2.FONT_HERSHEY_SIMPLEX)
+            cv2.resize(image_np, (800,600))
+            service.data = data
+            _, jpeg_bytes_tmp = cv2.imencode('.jpg', image_np) # to jpeg
+            service.jpeg_bytes = jpeg_bytes_tmp.tobytes()
+            
+            cond.acquire()
+            cond.notifyAll()
+            cond.release()
 
-      # yang ini buat count objectnya
-      # 1: person, 2: bicycle, 3: car, 4: motorcycle, 6: bus, 8: truck
-      data = {}  
-      data['vessel'] = []
-      s_class = classes[scores > 0.5]
-      # print(str(len(s_class))+ ' object')
-      # for i in range(len(s_class)): print(category_index.get(s_class[i])['name'])
-      if(len(s_class)!=0 and s_class[0] == 3):
-        cv2.putText(image_np,"Detected Car: " + str(len(s_class)), (10, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255),2,cv2.FONT_HERSHEY_SIMPLEX)
-        # save output to json
-        data['vessel'].append({  
-          'id': 3,
-          'object': 'car',
-          'sum': str(len(s_class))
-        })
-      elif(len(s_class)!=0 and s_class[0] == 1):
-        cv2.putText(image_np,"Detected Person: " + str(len(s_class)), (10, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255),2,cv2.FONT_HERSHEY_SIMPLEX)
-      else:
-        cv2.putText(image_np,"Detected Object: " + str(len(s_class)), (10, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255),2,cv2.FONT_HERSHEY_SIMPLEX)
-      cv2.resize(image_np, (800,600))
-      service.data = data
-      _, jpeg_bytes_tmp = cv2.imencode('.jpg', image_np) # to jpeg
-      service.jpeg_bytes = jpeg_bytes_tmp.tobytes()
-      
-      cond.acquire()
-      cond.notifyAll()
-      cond.release()
+if __name__ == "__main__":
+    main()
